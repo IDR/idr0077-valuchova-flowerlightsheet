62c442dc9fb595958675f69b389bd962aaa4fc45
William Moore <w.moore@dundee.ac.uk>
2020-04-23 14:37:06 +0100

    Handle replacements and Max Projections

--- files_to_bulk_tsv.py
+++ files_to_bulk_tsv.py
@@ -5,44 +5,65 @@ import re
 project = "idr0077-valuchova-flowerlightsheet/experimentA"
 path_to_data = "/uod/idr/filesets/idr0077-valuchova-flowerlightsheet"
 
-# TODO: Unclear how to handle submitter instructions to ignore and replace??
+# TODO: Handle submitter instructions to ignore and replace...
 to_ignore = [
-    'DIC after.czi'
-    '2019-01-17 DR5 stages 03.czi',
+    'DIC after.czi'                     # from Supplemental toxicity
+    '2019-01-17 DR5 stages 03.czi',     # from figure 4
+    # we don't have these other 3
     '2019-01-17 DR5 stages 05b.czi',
     '2019-01-17 DR5 stages 07 weaker.czi',
     '2019-01-17 DR5 stages 09 ewaker.czi',
 ]
-replace_with = [
+replace_in_figure_4 = [
     '2019-01-16 DR5 nls 07b.czi',
     '2019-01-16 DR5 nls 03.czi',
-    '2019-01-16 DR5 nls 09.czi'
+    '2019-01-16 DR5 nls 09.czi',
+    '2019-01-16 DR5 nls 05.czi'
 ]
 
-with open('idr0077-experimentA-filePaths.tsv', mode='w') as tsv_file:
-    tsv_writer = csv.writer(tsv_file, delimiter='\t')
-    # iterate all images...
-    for root, dirs, files in os.walk(path_to_data):
-        archive_root = os.path.relpath(root, path_to_data)
-        imgs_in_dir = []
-        for f in files:
-            # We want '.czi' files but not all ...(123).czi slices
-            if f.endswith('.czi') and '(' not in f and f not in to_ignore:
-                imgs_in_dir.append(f)
-        if len(imgs_in_dir) == 0:
-            # didn't find any files when we exclude '('
-            # Try to allow '(1).czi'
-            for f in files:
-                if f.endswith('(1).czi'):
-                    imgs_in_dir.append(f)
-        for f in imgs_in_dir:
+# iterate all images...
+imgs_to_dataset = {}
+tsv_rows = []
+projections = []
+for root, dirs, files in os.walk(path_to_data):
+    for f in files:
+        # We want '.czi' files but not any ...(n).czi slices
+        if f.endswith('.czi') and '(' not in f and f not in to_ignore:
             fullpath = os.path.join(root, f)
             match = re.search(r'^.*(figure 0\d).*', fullpath)
             if match:
-                dataset = match.group(1).title()
-            else:
+                # 'figure 01' -> 'Figure 1'
+                dataset = match.group(1).title().replace(' 0', ' ')
+            elif f == "2019-01-11 ASY H2B a 60min 03bud.czi":
+                # Special case: handle ftp uploads
+                dataset = "Figure 3"
+            elif f in replace_in_figure_4:
+                # Special case: handle ftp uploads
+                dataset = "Figure 4"
+            elif "Supplement toxicity" in fullpath:
                 dataset = "Supplement toxicity"
-            # Project:name:idr0077-valuchova-flowerlightsheet/Dataset:name:Figure 1
-            target = "Project:name:%s/Dataset:name:%s" % (project, dataset)
+            else:
+                dataset = "???"
             print(target, fullpath, f)
-            tsv_writer.writerow([target, fullpath, f])
+            f.endswith("Maximum intensity projection.czi"):
+                projections.append([dataset, fullpath, f])
+            else:
+                imgs_to_dataset[f] = dataset
+                tsv_rows.append([dataset, fullpath, f])
+
+# Use imgs_to_dataset mapping to assign Projections...
+for row in projections:
+    dataset, fullpath, f = row
+    name = f.replace("Maximum intensity projection.czi", "")
+    if name in imgs_to_dataset:
+        dataset = imgs_to_dataset[name]
+    tsv_rows.append([dataset, fullpath, f])
+
+# write to .tsv
+with open('idr0077-experimentA-filePaths.tsv', mode='w') as tsv_file:
+    tsv_writer = csv.writer(tsv_file, delimiter='\t')
+    # Project:name:idr0077-valuchova-flowerlightsheet/Dataset:name:Figure 1
+    for row in tsv_rows:
+        dataset, fullpath, f = row
+        target = "Project:name:%s/Dataset:name:%s" % (project, dataset)
+        tsv_writer.writerow([target, fullpath, f])
